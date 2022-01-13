8d8aeb31eb5cdd497b0e3cc5b68b1d85af3ad2f7
Todd Romkema <todd.romkema@gmail.com>
2022-01-13 16:08:55 -0600

    Finished Part 1

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,5 +1,21 @@
 --Part 1
 
+-- My query was 
+SELECT *
+FROM jobs;
+
+-- followed by
+SELECT DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_schema = 'techjobs' and table_name='jobs';
+
+/*My answer is
+Id - int
+Name - longtext
+EmployerId - int*/
+
+
+
 --Part 2
 
 --Part 3
